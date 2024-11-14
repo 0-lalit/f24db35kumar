8f0423893025154c182647a66f7485dc7c1600ff
Lalit Kumar <lalitkumar9684@gmail.com>
2024-11-13 19:03:25 -0600

    Update resource.js

--- routes/resource.js
+++ routes/resource.js
@@ -7,22 +7,22 @@ var electronics_controller = require('../controllers/electronics');
 
 /// API ROUTE ///
 // GET resources base
-router.get('/', api_controller.api);
+// router.get('/', api_controller.api);
 
 /// ELECTRONICS ROUTES ///
 // POST request for creating an Electronics item
-router.post('/electronics', electronics_controller.electronics_create_post);
+router.post('/', electronics_controller.electronics_create_post);
 
 // DELETE request to delete an Electronics item
-router.delete('/electronics/:id', electronics_controller.electronics_delete);
+router.delete('/:id', electronics_controller.electronics_delete);
 
 // PUT request to update an Electronics item
-router.put('/electronics/:id', electronics_controller.electronics_update_put);
+router.put('/:id', electronics_controller.electronics_update_put);
 
 // GET request for one Electronics item
-router.get('/electronics/:id', electronics_controller.electronics_detail);
+router.get('/:id', electronics_controller.electronics_detail);
 
 // GET request for list of all Electronics items
-router.get('/electronics', electronics_controller.electronics_list);
+router.get('/', electronics_controller.electronics_list);
 
 module.exports = router;
