f71b0aa0c4b2d56df3cc4461b7a5c8a0950a2007
Lalit Kumar <lalitkumar9684@gmail.com>
2024-11-20 18:11:20 -0600

    detail view

--- routes/electronics.js
+++ routes/electronics.js
@@ -1,5 +1,6 @@
 var express = require('express');
-const electronics_controller = require('../controllers/electronics');  // Changed to electronics_controller
+const electronics_controller = require('../controllers/electronics'); 
+const details_controller = require('../controllers/details_controller'); 
 var router = express.Router();
  
 // GET request to fetch all electronics
@@ -7,5 +8,7 @@ router.get('/', electronics_controller.electronics_list);
  
 // POST request to create a new electronic item
 router.post('/electronics', electronics_controller.electronics_create_post);
+
+router.get('/detail', details_controller.electronics_view_one_Page);
 
 module.exports = router;
